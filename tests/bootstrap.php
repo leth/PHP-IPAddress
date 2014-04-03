601f13bf36df8feccff26b68690ad6108ea545ea
Marcus Cobden <git@marcuscobden.co.uk>
2014-04-03 22:37:10 +0100

    Use Composer autoloader for PHPUnit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,7 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-function autoload($class_name) {
-	$file = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
-	$file = str_replace('_', DIRECTORY_SEPARATOR, $file).'.php';
-	$local_file = 'classes/'.$file;
+require __DIR__.'/../vendor/autoload.php';
 
-	if (file_exists($local_file))
-		include_once $local_file;
-	else
-		include_once $file;
-}
-
-spl_autoload_register('autoload');
+// @todo This shouldn't be needed.
+require 'vendor/pear/math_biginteger/BigInteger.php';
