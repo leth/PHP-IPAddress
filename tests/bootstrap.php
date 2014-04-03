5b9cc0d4f4ac78adf88f31375de82587820cc798
Marcus Cobden <git@marcuscobden.co.uk>
2014-04-03 22:39:44 +0100

    Add BigInteger backend modes to test matrix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 // @todo This shouldn't be needed.
 require 'vendor/pear/math_biginteger/BigInteger.php';
+
+$bigint_mode = getenv('MATH_BIGINTEGER_MODE');
+if ($bigint_mode !== FALSE)
+	define('MATH_BIGINTEGER_MODE', constant('MATH_BIGINTEGER_MODE_'.$bigint_mode));
