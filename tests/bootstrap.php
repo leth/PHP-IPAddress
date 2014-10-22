2e24cf0f73633e5312357ed9b6ef891429b304f1
Doug Tabacco <tabacco@dougtabacco.com>
2014-10-22 16:24:44 -0700

    Remove now-surplus require from test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,6 @@ error_reporting(E_ALL | E_STRICT);
 
 require __DIR__.'/../vendor/autoload.php';
 
-// @todo This shouldn't be needed.
-require 'vendor/pear/math_biginteger/BigInteger.php';
-
 $bigint_mode = getenv('MATH_BIGINTEGER_MODE');
 if ($bigint_mode !== FALSE)
 	define('MATH_BIGINTEGER_MODE', constant('MATH_BIGINTEGER_MODE_'.$bigint_mode));
